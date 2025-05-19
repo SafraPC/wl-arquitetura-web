eabbcfc31448bfa1092405794cff287ea556df6d
Leandro Safra <leandrosafra.jconectada@gmail.com>
2025-05-19 18:25:50 -0300

    feat: create project with next

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg">This is a sample Next.js application.</p>
+      <p className="text-lg">It uses Tailwind CSS for styling.</p>
+      <p className="text-lg">Enjoy coding!</p>
+    </div>
+  );
+}
